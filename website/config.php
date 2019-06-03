af963fa6b92ddc4b6251a9d08c7c24e34acf6322
Łukasz Moskała <lm@lukaszmoskala.pl>
2019-06-03 21:55:12 +0200

    Force language

--- website/config.php
+++ website/config.php
@@ -10,7 +10,11 @@ $DB_USER = "dht";
 $DB_PASS = "";
 $DB_DATABASE = "dht";
 
+//default lang specifies language to use, if it isn't specified by user
+//in GET request
+//force lang set to true ignores user preferences and uses default
 $DEFAULT_LANG = 'pl';
+$FORCE_LANG = false;
 
 //60 000 ms is 60 seconds
 //300 000 ms is 5 minutes
@@ -18,7 +22,7 @@ $AUTOREFRESH_DELAY = 60000;
 
 $used_lang=$DEFAULT_LANG;
 
-if(isset($_GET['lang'])) {
+if(isset($_GET['lang']) && !$FORCE_LANG) {
   $l = $_GET['lang'];
   //remove characters that could lead to execution of
   //unwanted files
@@ -31,7 +35,7 @@ if(isset($_GET['lang'])) {
   }
 }
 
-if(!isset($LANG_TITLE)) {
+if(!isset($LANG_TITLE) || $FORCE_LANG) {
   //lang not loaded, load default
   require "lang-$DEFAULT_LANG.php";
 }
